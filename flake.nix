9a3b18e870541420560eac57cc34c7b8d3e59688
tchz <theodoros.chatzi@tremau.com>
2024-06-23 13:43:01 +0300

    moving things around

--- flake.nix
+++ flake.nix
@@ -13,16 +13,28 @@
   };
 
   outputs = { self, nixpkgs, arcade-grub-theme }@inputs:
+    let
+      mkSystem = conf: nixpkgs.lib.nixosSystem {
+        modules = [
+          {
+            _module.args = inputs;
+          }
+          conf
+        ];
+      };
+    in
     {
     nixosConfigurations = {
-      tchz-thinkpad = nixpkgs.lib.nixosSystem {
+      tchz-yoga260 = mkSystem ./hosts/tchz-yoga260;
+
+      tchz-t480 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           {
             _module.args = inputs;
           }
           ./configuration.nix
-          ./hardware-configuration.nix
+          ./tchz-t480/hardware-configuration.nix
         ];
       };
     };
--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -63,7 +63,7 @@
 
 
   
-  networking.hostName = "tchz-thinkpad"; # Define your hostname.
+  # networking.hostName = "tchz-thinkpad"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -327,5 +327,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  # system.stateVersion = "23.05"; # Did you read the comment?
 }
