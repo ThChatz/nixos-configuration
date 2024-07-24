40be053d29efebb86ca41e198d2c94dd0afd9e11
name <temp@temp.com>
2024-07-24 22:25:40 +0300

    bootstrapping tchz-t480

--- common/agenix/secrets.nix
+++ common/agenix/secrets.nix
@@ -5,5 +5,7 @@ in
   "tchz-password-hash.age".publicKeys = [
     public-keys.tchz-yoga260.system
     public-keys.tchz-yoga260.users.tchz
+    public-keys.tchz-t480.system
   ];
 }
+# `agenix -r` to rekey :)
