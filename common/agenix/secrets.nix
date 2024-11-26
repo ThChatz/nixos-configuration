b0e65a01d2916a2bd6644993a56f008c1c9b4802
Theo <hthodoris@gmail.com>
2024-11-26 23:50:56 +0200

    tchz-vospro5402 public keys

--- common/agenix/secrets.nix
+++ common/agenix/secrets.nix
@@ -7,6 +7,8 @@ in
     public-keys.tchz-yoga260.users.tchz
     public-keys.tchz-t480.system
     public-keys.tchz-t480.users.tchz
+    public-keys.tchz-vospro5402.system
+    public-keys.tchz-vospro5402.users.theo
   ];
 }
 # `agenix -r` to rekey :)
